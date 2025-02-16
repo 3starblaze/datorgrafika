56e4fa10e5ad5bddaf209501509677be6e60857f
3starblaze <3starblaze@protonmail.com>
2025-02-16 16:30:46 +0200

    WIP Task 1b implementation
    WIP because it's buggy.

--- src/pages/task1b.tsx
+++ src/pages/task1b.tsx
@@ -1,5 +1,114 @@
 import { useEffect, useRef } from "react";
 
+interface Vector2 {
+    x: number,
+    y: number,
+};
+
+type ValueGenerator<Value> = (seed: number) => Value;
+
+interface ParticleSystem {
+    /**
+     * Single particle's visibility duration (seconds).
+     */
+    lifetime: number,
+    /**
+     * Linear velocity that the particle starts with (px/s).
+     */
+    linearVelocityGenerator: ValueGenerator<Vector2>,
+    /**
+     * Maximum particle count.
+     */
+    maxCount: number,
+    /**
+     * Particle spawn position.
+     */
+    origin: Vector2,
+};
+
+interface ParticleSystemState {
+    particleSystem: ParticleSystem,
+    particles: Particle[],
+    lastSpawnTime: number,
+};
+
+interface Particle {
+    birthTime: number,
+    position: Vector2,
+    linearVelocity: Vector2,
+};
+
+const applyLinearVelocity = function(
+    particle: Particle,
+    delta: number,
+): Particle {
+    const position = {
+        x: particle.position.x + particle.linearVelocity.x * delta,
+        y: particle.position.y + particle.linearVelocity.y * delta,
+    };
+
+    return {
+        ...particle,
+        position,
+    };
+};
+
+const initParticleSystemState = function(
+    particleSystem: ParticleSystem,
+): ParticleSystemState {
+    return {
+        particleSystem,
+        particles: [],
+        lastSpawnTime: 0,
+    };
+};
+
+const tick = function(
+    state: ParticleSystemState,
+    delta: number,
+): void {
+    const { particleSystem } = state;
+
+    const nowTime = performance.now() / 1000;
+
+    const spawnDelta = particleSystem.lifetime / particleSystem.maxCount;
+    const deathThreshold = nowTime - particleSystem.lifetime;
+
+    const processedParticles: Particle[] = state.particles
+        .map((particle) => applyLinearVelocity(particle, delta / 1000))
+        .filter((particle) => particle.birthTime > deathThreshold);
+
+    if ((nowTime - state.lastSpawnTime) >= spawnDelta) {
+        state.lastSpawnTime = nowTime;
+        state.particles = [
+            ...processedParticles,
+            {
+                birthTime: nowTime,
+                position: particleSystem.origin,
+                linearVelocity: particleSystem.linearVelocityGenerator(Math.random()),
+            },
+        ];
+    } else {
+        state.particles = processedParticles;
+    }
+};
+
+const draw = function(
+    state: ParticleSystemState,
+    size: Vector2,
+    ctx: CanvasRenderingContext2D,
+): void {
+    // NOTE: Background
+    ctx.fillStyle = "#aa0000";
+    ctx.fillRect(0, 0, size.x, size.y);
+
+    ctx.fillStyle = "#000000";
+
+    state.particles.forEach((particle) => {
+        ctx.fillRect(particle.position.x, particle.position.y, 10, 10);
+    });
+};
+
 export default function () {
     const canvasEl = useRef<HTMLCanvasElement>(null);
 
@@ -7,8 +116,33 @@ export default function () {
         if (!canvasEl.current) return;
         const ctx = canvasEl.current.getContext("2d");
         if (!ctx) return;
-        ctx.fillStyle = "#aa0000";
-        ctx.fillRect(0, 0, canvasEl.current.width, canvasEl.current.height);
+
+        const particleSystem: ParticleSystem = {
+            lifetime: 2.0,
+            linearVelocityGenerator: (seed) => {
+                const angle = seed * 2 * Math.PI;
+                return {
+                    x: 50 * Math.cos(angle),
+                    y: -50 * Math.sin(angle),
+                };
+            },
+            maxCount: 100,
+            origin: {x: 100, y: 100},
+        };
+
+        const state = initParticleSystemState(particleSystem);
+        const size = {
+            x: canvasEl.current.width,
+            y: canvasEl.current.height,
+        };
+
+        const callback = (delta: number) => {
+            tick(state, delta / 1000);
+            draw(state, size, ctx);
+            requestAnimationFrame(callback);
+        };
+
+        requestAnimationFrame(callback);
     }, []);
 
     return (
@@ -17,10 +151,8 @@ export default function () {
 
             <canvas
                 ref={canvasEl}
-                style={{
-                    width: 100,
-                    height: 100,
-                }}
+                width={400}
+                height={400}
             >
             </canvas>
         </div>
