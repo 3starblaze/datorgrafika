02d89cf88cc1547838231dd361db7283db12d6ef
Deniss Fjodorovs <3starblaze@protonmail.com>
2025-04-29 09:16:34 +0300

    Do DFT, IDFT in separate workers and cleanup logic
    This commit includes a handy "MaybeCanvas" component that handles
    loading state and canvas syncing which cleans up the main Task component.

--- src/pages/task2a/index.tsx
+++ src/pages/task2a/index.tsx
@@ -2,13 +2,18 @@ import { RefObject, useEffect, useMemo, useRef, useState } from "react";
 import sampleImg from "./sample_screenshot.png";
 import MainWorker from "./main_worker?worker";
 import {
-    FourierTransformRequest,
-    InverseFourierTransformRequest,
+    WorkerRequest,
     WorkerResponse,
 } from "./main_worker";
 import { rgbToGray } from "./util";
 
+/**
+ * Type for reporting values that can have "loading" state.
+ *
+ * This type is needed because my (I)DFT implementation is slow enough to warrant a loading message.
+ */
 type MaybeLoading<T> = null | "loading" | T;
+type State<T> = [T, (value: T) => void];
 
 const useBindImageToCanvas = function (
     imageData: MaybeLoading<ImageData>,
@@ -32,13 +37,70 @@ const useBindImageToCanvas = function (
     }, [canvasRef, imageData]);
 };
 
-export default function Task() {
-    const [imageData, setImageData] = useState<null | ImageData>(null);
+const useWorker = function(onmessage: Worker["onmessage"]) {
     const worker = useRef<Worker>(null);
 
+    useEffect(() => {
+        worker.current = new MainWorker();
+        worker.current.onmessage = onmessage;
+        return () => {
+            // NOTE: Worker should be destroyed on unmount to prevent useless work being done
+            worker.current?.terminate();
+        };
+    }, []);
+
+    return worker
+};
+
+
+const useScheduleWorkerAction = function<T>(
+    sourceImageData: ImageData | null,
+    workerRef: RefObject<Worker | null>,
+    state: State<MaybeLoading<T>>,
+    messageConstructor: (imageData: ImageData) => WorkerRequest,
+): void {
+    useEffect(() => {
+        if (sourceImageData === null) {
+            state[1](null);
+            return;
+        }
+
+        if (!workerRef.current) return;
+
+        state[1]("loading");
+
+        workerRef.current.postMessage(messageConstructor(sourceImageData));
+    }, [sourceImageData]);
+};
+
+const MaybeCanvas = function({
+    maybeImageData,
+    loadingText,
+}: {
+    maybeImageData: MaybeLoading<ImageData>,
+    loadingText: string,
+}) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useBindImageToCanvas(maybeImageData, canvasRef);
+
+    return (
+        <div>
+            {(maybeImageData === "loading") && (
+                <div className="flex gap-4 mt-2 items-center">
+                    <p className="text-gray-500">{loadingText}</p>
+                    <div className="h-4 w-4 bg-gray-500 animate-spin"></div>
+                </div>
+            )}
+            <canvas ref={canvasRef} />
+        </div>
+    );
+};
+
+export default function Task() {
+    const [imageData, setImageData] = useState<null | ImageData>(null);
+
     const grayscaleCanvasRef = useRef<HTMLCanvasElement>(null);
-    const transformCanvasRef = useRef<HTMLCanvasElement>(null);
-    const inverseTransformCanvasRef = useRef<HTMLCanvasElement>(null);
 
     const grayscaleImageData = useMemo<ImageData | null>(() => {
         if (imageData === null) return null;
@@ -62,63 +124,49 @@ export default function Task() {
         return res;
     }, [imageData]);
 
-    const [fourierTransformData, setFourierTransformData] = useState<MaybeLoading<ImageData>>(null);
-    const [inverseFourierTransformData, setInverseFourierTransformData] =
-        useState<MaybeLoading<ImageData>>(null);
+    const fourierTransformDataState = useState<MaybeLoading<ImageData>>(null);
+    const [fourierTransformData, setFourierTransformData] = fourierTransformDataState;
 
-    // NOTE: Worker setup
-    useEffect(() => {
-        worker.current = new MainWorker();
-        worker.current.onmessage = ({data}: MessageEvent<WorkerResponse>) => {
-            switch (data.id) {
-                case "fourier_transform_result":
-                    setFourierTransformData(data.imageData);
-                    break;
-                case "inverse_fourier_transform_result":
-                    setInverseFourierTransformData(data.imageData);
-                    break;
-            }
-        };
-        return () => {
-            // NOTE: Worker should be destroyed on unmount to prevent useless work being done
-            worker.current?.terminate();
-        };
-    }, []);
+    const inverseFourierTransformDataState = useState<MaybeLoading<ImageData>>(null);
+    const [inverseFourierTransformData, setInverseFourierTransformData] = inverseFourierTransformDataState;
 
-    // NOTE: Effect that starts Fourier transform when grayscale image is available
-    useEffect(() => {
-        if (grayscaleImageData === null) {
-            setFourierTransformData(null);
-            setInverseFourierTransformData(null);
-            return;
+    const transformWorker = useWorker(({ data }: MessageEvent<WorkerResponse>) => {
+        switch (data.id) {
+            case "fourier_transform_result":
+                setFourierTransformData(data.imageData);
+                break;
         }
-        if (!worker.current) return;
+    });
 
-        setFourierTransformData("loading");
-        setInverseFourierTransformData("loading");
-
-        const sourceImageData = grayscaleImageData;
-
-        const transformMessage: FourierTransformRequest = {
+    useScheduleWorkerAction(
+        grayscaleImageData,
+        transformWorker,
+        fourierTransformDataState,
+        (data) => ({
             id: "fourier_transform_request",
-            sourceImageData,
-        };
-        worker.current.postMessage(transformMessage);
+            sourceImageData: data,
+        })
+    );
 
-        const inverseTransformMessage: InverseFourierTransformRequest = {
+    const inverseTransformWorker = useWorker(({ data }: MessageEvent<WorkerResponse>) => {
+        switch (data.id) {
+            case "inverse_fourier_transform_result":
+                setInverseFourierTransformData(data.imageData);
+                break;
+        }
+    });
+
+    useScheduleWorkerAction(
+        grayscaleImageData,
+        inverseTransformWorker,
+        inverseFourierTransformDataState,
+        (data) => ({
             id: "inverse_fourier_transform_request",
-            sourceImageData,
-        };
-        worker.current.postMessage(inverseTransformMessage);
-
-    }, [grayscaleImageData]);
-
+            sourceImageData: data,
+        }),
+    );
 
     useBindImageToCanvas(grayscaleImageData, grayscaleCanvasRef);
-
-    useBindImageToCanvas(fourierTransformData, transformCanvasRef);
-
-    useBindImageToCanvas(inverseFourierTransformData, inverseTransformCanvasRef);
 
     useEffect(() => {
         (async () => {
@@ -150,22 +198,16 @@ export default function Task() {
             <canvas ref={grayscaleCanvasRef} />
 
             <p className="text-lg">Attēls, kuram pielietota Furjē transformācija</p>
-            {(fourierTransformData === "loading") && (
-                <div className="flex gap-4 mt-2 items-center">
-                    <p className="text-gray-500">Transformācija tiek veikta, lūdzu uzgaidiet...</p>
-                    <div className="h-4 w-4 bg-gray-500 animate-spin"></div>
-                </div>
-            )}
-            <canvas ref={transformCanvasRef} />
+            <MaybeCanvas
+                maybeImageData={fourierTransformData}
+                loadingText="Transformācija tiek veikta, lūdzu uzgaidiet..."
+            />
 
             <p className="text-lg">Attēls, kuram pielietota inversā Furjē transformācija</p>
-            {(inverseFourierTransformData === "loading") && (
-                <div className="flex gap-4 mt-2 items-center">
-                    <p className="text-gray-500">Inversā transformācija tiek veikta, lūdzu uzgaidiet...</p>
-                    <div className="h-4 w-4 bg-gray-500 animate-spin"></div>
-                </div>
-            )}
-            <canvas ref={inverseTransformCanvasRef} />
+            <MaybeCanvas
+                maybeImageData={inverseFourierTransformData}
+                loadingText="Inversā transformācija tiek veikta, lūdzu uzgaidiet..."
+            />
         </div>
     );
 };
